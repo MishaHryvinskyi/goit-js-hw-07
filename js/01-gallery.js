e4025ae5e885f189a1b99a446c0bbc70ef289c57
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-03-23 18:57:24 +0200

    marcup gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarcup = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img 
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+}).join(' ');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarcup);
+
+
+console.log(galleryList);
 
 console.log(galleryItems);
+
