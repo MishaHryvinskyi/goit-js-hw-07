8c265376a464e7ae424206df0d2fdbdb23100f5e
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-03-24 22:11:39 +0200

    completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryList = document.querySelector(".gallery");
+const cardItem = createListElement(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', cardItem);
+
+function createListElement(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img 
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            data-source="${original}"
+            />
+        </a>
+    </li>`;
+}).join(' ');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
